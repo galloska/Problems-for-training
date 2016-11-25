95b5a211d65f8c2c94c425e293259a25a88ed356
galloska <pockeetgym@gmail.com>
2016-11-25 13:12:09 -0800

    Adding Relative.cpp

---
+++ Math/Relatives.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define lli long long int
+
+using namespace std;
+
+lli EulerPhi(lli n){
+	if(n==1) return 0ll;
+	lli res = n;
+	for(lli i=2;i*i<=n;i++)
+		if(n%i==0){
+			while(n%i==0) n /= i;
+			res -= res / i;
+		}
+	if(n>1) res -= res / n;
+	return res;
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	lli n;
+	while(cin>>n && n){
+		cout<<EulerPhi(n)<<endl;
+	}
+	return 0;
+}
